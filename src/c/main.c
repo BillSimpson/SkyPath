a5a9b993584c629660fe8477385816e12b1df2d2
BillSimpson <w.r.simpson@gmail.com>
2016-11-24 23:18:56 -0900

    last version before adding solar / lunar calcs
    This version has static diurnal cycles.  Now fixing that aspect in the next version

--- src/c/main.c
+++ src/c/main.c
@@ -1,20 +1,21 @@
 #include <pebble.h>
 //
-// A simple test to calculate solar and lunar elevation and azimuth
-// 
-// Based upon suncalc java script code.  
-//
-// https://github.com/mourner/suncalc/blob/master/suncalc.js
+// First attempt at the sunpath watchface
 //
 static Window *s_main_window;
 // set up text layers for time and SZA information
 static TextLayer *s_time_layer;
-static TextLayer *s_sza_layer;
+static TextLayer *s_date_layer;
 // set up canvas layer for drawing
 static Layer *s_canvas_layer;
+// set up sun bitmaps
+static GBitmap *s_bitmap_sun_rim;
+static GBitmap *s_bitmap_horizon;
 
 // Global variables
 float solar_elev[] = { -14, -12, -9, -6, -3, 0, 3, 6, 8, 10, 12, 13, 14, 14, 13, 12, 10, 8, 6, 3, 0, -3, -6, -9, -12, -14 };
+float lunar_elev[] = { 0, 9, 18, 24, 30, 36, 39, 42, 43, 39, 36, 30, 24, 18, 9, 0, -3, -6, -9, -12, -14, -14, -12, -9, -6, -3 };
+float lunar_azi_hour[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
 float graph_width, graph_height;
 float curr_solar_elev;
 
@@ -25,7 +26,7 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
-  static char sza_buffer[10];
+  static char s_date_buffer[12];
   
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
@@ -33,9 +34,9 @@ static void update_time() {
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
   
-  // Update the solar elevation 
-  snprintf(sza_buffer, 9, "SunEl %d",(int)curr_solar_elev);
-  text_layer_set_text(s_sza_layer, sza_buffer);
+  // Update the date text  
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%Y-%b-%e", tick_time);
+  text_layer_set_text(s_date_layer, s_date_buffer);
   
 }
 
@@ -49,38 +50,41 @@ int hour_to_xpixel (float hour) {
 }
 
 int angle_to_ypixel (float angle) {
-  // vertical = 100 degrees, with 90 on top, -10 at bottom
-  return (int)((90-angle)/100 * graph_height);
+  // vertical = 110 degrees, with 90 on top, -20 at bottom
+  return (int)((90-angle)/110 * graph_height);
 }
 
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
   // Custom drawing happens here!
   int i;
-  GPoint start, end;
+  GPoint dot;
   
   // Disable antialiasing (enabled by default where available)
   // graphics_context_set_antialiased(ctx, false);
   
   // Set the line color
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  
-  // Set the fill color
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  
-  // Generate the horizon box from 0 to -10 degrees elevation
-  GRect horizon_box = GRect(hour_to_xpixel(0),angle_to_ypixel(0),
-                            hour_to_xpixel(24),angle_to_ypixel(-10));
-  // Draw the horizon box
-  graphics_fill_rect(ctx, horizon_box, 0, GCornerNone);
-
   // Set the stroke width (must be an odd integer value)
   graphics_context_set_stroke_width(ctx, 1);
+  // Set the fill color
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  // Set the compositing mode (GCompOpSet is required for transparency)
+  graphics_context_set_compositing_mode(ctx, GCompOpSet);
+  
+  // Generate the horizon 
+  GRect horizon_box = GRect(hour_to_xpixel(0),angle_to_ypixel(0),hour_to_xpixel(24),7);
+  // Draw the horizon box
+  graphics_draw_bitmap_in_rect(ctx, s_bitmap_horizon, horizon_box);
 
+  // Draw solar path
   for (i=0;i<24;i++) {
-    start = GPoint(hour_to_xpixel(i),angle_to_ypixel(solar_elev[i]));
-    end = GPoint(hour_to_xpixel(i+1),angle_to_ypixel(solar_elev[i+1]));
-    // Draw a line
-    graphics_draw_line(ctx, start, end);
+    dot = GPoint(hour_to_xpixel(i),angle_to_ypixel(solar_elev[i]));
+    if (solar_elev[i]>0) graphics_draw_pixel(ctx, dot);
+  }
+  // Draw lunar path
+    for (i=0;i<24;i++) {
+    dot = GPoint(hour_to_xpixel(lunar_azi_hour[i]),angle_to_ypixel(lunar_elev[i]));
+    if (lunar_elev[i]>0) graphics_draw_pixel(ctx, dot);
   }
   
   // Calculate sun position
@@ -91,22 +95,13 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   float frac_hour = ((float)curr_time->tm_min)/60;
   curr_solar_elev = solar_elev[hour] + frac_hour * (solar_elev[hour+1]-solar_elev[hour]);
   float curr_elev = curr_solar_elev;
-  if (curr_elev>0) {
-    // Daytime (or twilight). Set the fill color white
-    graphics_context_set_fill_color(ctx, GColorWhite);
-  }
-  else {
-    // Nighttime. Set the fill color black
-    graphics_context_set_fill_color(ctx, GColorBlack);
-    if (curr_elev < -5) curr_elev = -5;
-  }
-  // make the radius 5 degrees in y coordinate
-  int radius = angle_to_ypixel(-5) - angle_to_ypixel(0);
-  GPoint center = GPoint(hour_to_xpixel(hour+frac_hour),angle_to_ypixel(curr_elev));
-  // draw border
-  graphics_draw_circle(ctx, center, radius);
-  // draw fill 
-  graphics_fill_circle(ctx, center, radius-1);
+ 
+  // If sun is too low, stop lowering its position
+  if (curr_elev < -9) curr_elev = -9;
+  // Get the location to place the sun
+  GRect bitmap_placed = GRect(hour_to_xpixel(hour+frac_hour)-7,angle_to_ypixel(curr_elev)-6,15,13);
+  // Draw the image
+  graphics_draw_bitmap_in_rect(ctx, s_bitmap_sun_rim, bitmap_placed);
 }
 
 static void main_window_load(Window *window) {
@@ -114,11 +109,11 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // create drawing canvas for data visualization -- top half of display
+  // create drawing canvas for data visualization -- top 40% of display
   s_canvas_layer = layer_create(
-      GRect(0, 0, bounds.size.w, bounds.size.h/2));
+      GRect(0, 0, bounds.size.w, bounds.size.h*0.4));
   graph_width = bounds.size.w;
-  graph_height = bounds.size.h/2;
+  graph_height = bounds.size.h*0.4;
   
   // Assign the custom drawing procedure
   layer_set_update_proc(s_canvas_layer, canvas_update_proc);
@@ -128,7 +123,7 @@ static void main_window_load(Window *window) {
   
   // Create the TextLayer with specific bounds on bottom half of display
   s_time_layer = text_layer_create(
-      GRect(0, bounds.size.h/2, bounds.size.w, 50));
+      GRect(0, bounds.size.h*0.4, bounds.size.w, 45));
   
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorBlack);
@@ -139,29 +134,32 @@ static void main_window_load(Window *window) {
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   
-  // Create sza information layer
-  s_sza_layer = text_layer_create(
-    GRect(0, (bounds.size.h/2+51), bounds.size.w, 29));
+  // Create date information layer
+  s_date_layer = text_layer_create(
+    GRect(0, (bounds.size.h*0.4+45), bounds.size.w, 25));
 
   // Style the text
-  text_layer_set_background_color(s_sza_layer, GColorBlack);
-  text_layer_set_text_color(s_sza_layer, GColorWhite);
-  text_layer_set_text_alignment(s_sza_layer, GTextAlignmentCenter);
-  text_layer_set_font(s_sza_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_text(s_sza_layer, "Sky Path");
+  text_layer_set_background_color(s_date_layer, GColorBlack);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text(s_date_layer, "Date");
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_sza_layer));
-
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayers
   text_layer_destroy(s_time_layer);
-  text_layer_destroy(s_sza_layer);
+  text_layer_destroy(s_date_layer);
 
   // Destroy canvas
   layer_destroy(s_canvas_layer);
+  
+  // Destroy the image data
+  gbitmap_destroy(s_bitmap_sun_rim);
+  gbitmap_destroy(s_bitmap_horizon);
 }
 
 static void init() {
@@ -185,6 +183,11 @@ static void init() {
   
   // Set the window background to the image background
   window_set_background_color(s_main_window, GColorBlack);
+  
+  // load bitmaps
+  s_bitmap_sun_rim = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SUN_RIM);
+  s_bitmap_horizon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_HORIZON);
+
 }
 
 static void deinit() {
