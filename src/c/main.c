aca31db8884873ef3d01b315fbbe35e3102b7899
BillSimpson <w.r.simpson@gmail.com>
2016-11-26 23:34:34 -0900

    added moon image selection
    added moon image selection and solar information line at bottom.

--- src/c/main.c
+++ src/c/main.c
@@ -1,20 +1,28 @@
 #include <pebble.h>
 //
-// First attempt at the sunpath watchface
+// First attempt at the skypath (sun and moon) watchface "ephemeris"
 //
 static Window *s_main_window;
-// set up text layers for time and SZA information
+// set up text layers for time and date and info information
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
+static TextLayer *s_info_layer;
 // set up canvas layer for drawing
 static Layer *s_canvas_layer;
 // set up sun bitmaps
 static GBitmap *s_bitmap_sun_rim;
 static GBitmap *s_bitmap_horizon;
+static GBitmap *s_bitmap_moon;
 
 // Global variables
-float graph_width, graph_height;
-float curr_solar_elev;
+static float graph_width, graph_height;
+static float curr_solar_elev, curr_solar_azi, curr_lunar_elev, curr_lunar_azi;
+static float solar_elev[25];
+static float solar_azi[25];
+static float lunar_elev[25];
+static float lunar_azi[25];
+static int lunar_day;
+static float lat = 64.8, lng = -147;
 
 //
 // Adapted from the javascript code below to C
@@ -26,10 +34,6 @@ float curr_solar_elev;
 // SunCalc is a JavaScript library for calculating sun/moon position and light phases.
 // https://github.com/mourner/suncalc
 //
-#include <stdio.h>
-#include <math.h>
-#include <time.h>  
-
 // sun calculations are based on http://aa.quae.nl/en/reken/zonpositie.html formulas
 
 // date/time constants and conversions
@@ -42,9 +46,6 @@ float deg_conv = 180 / 3.14159268;
 
 float sin_pebble(float angle_radians) {
   int32_t angle_pebble = angle_radians * TRIG_MAX_ANGLE / (2*pi);
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sin argument angle_pebble = %ld", angle_pebble);
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sin lookup value = %ld", sin_lookup(angle_pebble));
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sin result * 100 %d", (int)(100*(float)sin_lookup(angle_pebble) / (float)TRIG_MAX_RATIO));
   return ((float)(sin_lookup(angle_pebble)) / (float)TRIG_MAX_RATIO);
 }
 
@@ -65,12 +66,13 @@ float atan2_pebble(float y, float x) {
   return (2*pi * (float)atan2_lookup(y_pebble, x_pebble) / (float)TRIG_MAX_ANGLE);
 }
 
-float fmod_pebble(float a, float d) {
+float fmod_pebble(float product, float divisor) {
+  int32_t factor;
   float remainder;
-  remainder = a;
-  while ( remainder > d ) {
-    remainder = remainder - d;
-  }
+  
+  factor = (int32_t)(product/divisor);
+  if (product<0) factor--;    // casting to int truncates so check if negative and decrement
+  remainder = product - ((float)factor)*divisor;
   return remainder;
 }
 
@@ -169,23 +171,23 @@ void moonPosition(time_t unixdate, float lat, float lng, float *azi, float *alt)
   *alt = h;
 };
 
+// Greatly simplified moon phase algorithm from
+// http://jivebay.com/calculating-the-moon-phase/
+// This simply takes a new moon and uses the moon cycle from there.
+// The function returns an integer between 0 and 29 that is the days
+// into the lunar cycle.  Thus, 0 is new moon, 15 is full moon, and 29 is new
 
-// calculations for illumination parameters of the moon,
-// based on http://idlastro.gsfc.nasa.gov/ftp/pro/astro/mphase.pro formulas and
-// Chapter 48 of "Astronomical Algorithms" 2nd edition by Jean Meeus (Willmann-Bell, Richmond) 1998.
-
-float moonPhase(time_t unixdate) {
+int moonPhase(time_t unixdate) {
   float d = toDays(unixdate);
-  float s_ra, s_dec, m_ra, m_dec;
-  sunCoords(d, &s_ra, &s_dec);
-  moonCoords(d, &m_ra, &m_dec);
-
-  float phase_angle = (m_ra-s_ra);
-  if (phase_angle < 0) phase_angle = phase_angle + 2*pi;
-  return fmod_pebble(phase_angle/(2*pi),1);
+  // 2015-Jan-01 had an 11-day old moon
+  // 2015-Jan-01 was 1420070400 seconds (unix time)
+  // and from earlier, "d" has zero at 946684800 seconds, so 
+  // the "d" value of 2015-Jan-01 = 5479, so there was a new moon 11 days earlier
+  // We start with the new moon on d=5468
+  return ((int)fmod_pebble((d-5468),29.5305882));
 }
 
-void sky_paths_today(float lat, float lng, float solar_elev[], float solar_azi[], float lunar_elev[], float lunar_azi[], float lunar_phase[]) {
+void sky_paths_today(float lat, float lng, float solar_elev[], float solar_azi[], float lunar_elev[], float lunar_azi[]) {
   float sol_alt, sol_azi, moon_alt, moon_azi;
   int i;
   
@@ -198,31 +200,52 @@ void sky_paths_today(float lat, float lng, float solar_elev[], float solar_azi[]
   curr_time->tm_hour = 0;
   temp = mktime(curr_time);
 
-  // cycle through 700 hours calculating solar and lunar parameters
+  // cycle through 25 hours calculating solar and lunar parameters
   for (i=0;i<25;i++) {
     // Solar calculation
     sunPosition(temp, lat, lng, &sol_azi, &sol_alt);
     solar_elev[i] = sol_alt * deg_conv;
     solar_azi[i] = fmod_pebble(((sol_azi + pi) * deg_conv ),360);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "hour %d Solar: Elev %d  Azi %d", i, (int)solar_elev[i], (int)solar_azi[i]);
+//    APP_LOG(APP_LOG_LEVEL_DEBUG, "hour %d Solar: Elev %d  Azi %d", i, (int)solar_elev[i], (int)solar_azi[i]);
 
     // Lunar calculation
     moonPosition(temp, lat, lng, &moon_azi, &moon_alt);
     lunar_elev[i] = moon_alt * deg_conv;
     lunar_azi[i] = fmod_pebble(((moon_azi + pi) * deg_conv ),360);
-    lunar_phase[i] = moonPhase(temp);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "       Lunar: Elev %d  Azi %d", (int)lunar_elev[i], (int)lunar_azi[i]);
-
+//    APP_LOG(APP_LOG_LEVEL_DEBUG, "       Lunar: Elev %d  Azi %d", (int)lunar_elev[i], (int)lunar_azi[i]);
+    
     // advance to the next hour
     temp = temp + 3600;
   }
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Re-calculated sky paths");
 }
 
-float solar_elev[25];
-float solar_azi[25];
-float lunar_elev[25];
-float lunar_azi[25];
-float lunar_phase[25];
+static void load_moon_image() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+
+  lunar_day = moonPhase(temp);
+  if (lunar_day < 3)
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON1);
+  if ((lunar_day < 6) && (lunar_day >= 3)) 
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON2);
+  if ((lunar_day < 10) && (lunar_day >= 6))  // half moon -- 4 days
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON3);
+  if ((lunar_day < 13) && (lunar_day >= 10)) 
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON4);
+  if ((lunar_day < 16) && (lunar_day >= 13)) 
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON5);
+  if ((lunar_day < 19) && (lunar_day >= 16)) 
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON6);
+  if ((lunar_day < 23) && (lunar_day >= 19)) // half moon -- 4 days
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON7);
+  if ((lunar_day < 26) && (lunar_day >= 23)) 
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON8);
+  if (lunar_day >= 26) 
+    s_bitmap_moon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MOON9);
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Selected moon image for lunar day (moon phase 0-29) %d", lunar_day);
+}
 
 static void update_time() {
   // Get a tm structure
@@ -232,17 +255,33 @@ static void update_time() {
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   static char s_date_buffer[12];
+  static char s_info_buffer[15];
   
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
+                                          "%k:%M" : "%l:%M", tick_time);
+//  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+//                                          "%H:%M" : "%I:%M", tick_time);
+
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
   
   // Update the date text  
   strftime(s_date_buffer, sizeof(s_date_buffer), "%Y-%b-%e", tick_time);
-//  snprintf(s_date_buffer, sizeof(s_date_buffer), "Sol El %d", (int)curr_solar_elev);
   text_layer_set_text(s_date_layer, s_date_buffer);
+  
+  // Update the info text  
+  snprintf(s_info_buffer, sizeof(s_info_buffer), "S [%d|%d]", (int)curr_solar_elev, (int)curr_solar_azi);
+  text_layer_set_text(s_info_layer, s_info_buffer);
+
+  // if it is an hour boundary, re-calculate the sun and moon ephemeris
+  if (tick_time->tm_min == 0) {
+    psleep(5000); // wait 5 seconds after the hour before re-calculating
+    // re-calculate sky paths
+    sky_paths_today(lat, lng, solar_elev, solar_azi, lunar_elev, lunar_azi);
+    // load a moon image (maybe a new one)
+    load_moon_image();
+  }
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -256,13 +295,17 @@ int hour_to_xpixel (float hour) {
 
 int angle_to_ypixel (float angle) {
   // vertical = 65 degrees, with 50 on top, -15 at bottom
-  return (int)((50-angle)/65 * graph_height);
+  int range = (90 - lat + 23.5) * 1.35;  // full graph 135% of the potential range at that lat
+  if (range>110) range = 110;
+  int top = (90 - lat + 23.5) * 1.05;  // this gives a 20% buffer below the horizon.
+  if (top>90) top = 90;
+  return (int)((top-angle)/range * graph_height);
 }
 
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
   // Custom drawing happens here!
   int i;
-  GPoint dot;
+  GPoint point1, point2;
   
   // Disable antialiasing (enabled by default where available)
   // graphics_context_set_antialiased(ctx, false);
@@ -283,13 +326,19 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
 
   // Draw solar path
   for (i=0;i<24;i++) {
-    dot = GPoint(hour_to_xpixel(i),angle_to_ypixel(solar_elev[i]));
-    if (solar_elev[i]>0) graphics_draw_pixel(ctx, dot);
+    point1 = GPoint(hour_to_xpixel(i),angle_to_ypixel(solar_elev[i]));
+    point2 = GPoint(hour_to_xpixel(i+1),angle_to_ypixel(solar_elev[i+1]));
+    if ((solar_elev[i]>0)||(solar_elev[i+1]>0) ) graphics_draw_line(ctx, point1, point2);
   }
   // Draw lunar path
-    for (i=0;i<24;i++) {
-    dot = GPoint(hour_to_xpixel(lunar_azi[i]/15),angle_to_ypixel(lunar_elev[i]));
-    if (lunar_elev[i]>0) graphics_draw_pixel(ctx, dot);
+  float curr_azi;
+  for (i=0;i<24;i++) {
+    curr_azi = lunar_azi[i] - solar_azi[0];
+    if (curr_azi > 360) curr_azi = curr_azi - 360;
+    if (curr_azi < 0 ) curr_azi = curr_azi + 360; 
+    point1 = GPoint(hour_to_xpixel(curr_azi/15),angle_to_ypixel(lunar_elev[i]));
+    point2 = GPoint(hour_to_xpixel(curr_azi/15+1),angle_to_ypixel(lunar_elev[i+1]));
+    if ((lunar_elev[i]>0)||(lunar_elev[i+1]>0)) graphics_draw_line(ctx, point1, point2);
   }
   
   // Calculate sun position
@@ -299,14 +348,34 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   int hour = curr_time->tm_hour;  
   float frac_hour = ((float)curr_time->tm_min)/60;
   curr_solar_elev = solar_elev[hour] + frac_hour * (solar_elev[hour+1]-solar_elev[hour]);
+  curr_solar_azi = solar_azi[hour] + 15 * frac_hour;
+  curr_solar_azi = fmod_pebble(curr_solar_azi,360);
   float curr_elev = curr_solar_elev;
  
   // If sun is too low, stop lowering its position
-  if (curr_elev < -9) curr_elev = -9;
+  if (curr_elev < -7) curr_elev = -7;
   // Get the location to place the sun
   GRect bitmap_placed = GRect(hour_to_xpixel(hour+frac_hour)-7,angle_to_ypixel(curr_elev)-6,15,13);
   // Draw the image
   graphics_draw_bitmap_in_rect(ctx, s_bitmap_sun_rim, bitmap_placed);
+
+
+  curr_lunar_elev = lunar_elev[hour] + frac_hour * (lunar_elev[hour+1]-lunar_elev[hour]);
+  curr_lunar_azi = lunar_azi[hour] + 15 * frac_hour;  
+  curr_lunar_azi = fmod_pebble(curr_lunar_azi,360);
+  curr_elev = curr_lunar_elev;
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Azi offset %d", (int)fmod_pebble(solar_azi[0],360));
+
+  curr_azi = lunar_azi[hour] + frac_hour * 15 - solar_azi[0];  // for display purposes
+  curr_azi = fmod_pebble(curr_azi,360);
+  // If moon is too low, stop lowering its position
+  if (curr_elev < -7) curr_elev = -7;
+  // Get the location to place the moon
+  GRect bitmap_moon_placed = GRect(hour_to_xpixel(curr_azi/15)-6,angle_to_ypixel(curr_elev)-6,13,13);
+  // Draw the image
+  graphics_draw_bitmap_in_rect(ctx, s_bitmap_moon, bitmap_moon_placed);
+
 }
 
 static void main_window_load(Window *window) {
@@ -352,12 +421,28 @@ static void main_window_load(Window *window) {
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+
+  // Create info layer
+  s_info_layer = text_layer_create(
+    GRect(0, (bounds.size.h*0.4+70), bounds.size.w, 25));
+
+  // Style the text
+  text_layer_set_background_color(s_info_layer, GColorBlack);
+  text_layer_set_text_color(s_info_layer, GColorWhite);
+  text_layer_set_text_alignment(s_info_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_info_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text(s_info_layer, "Sun Info");
+                                     
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_info_layer));
+
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayers
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
+  text_layer_destroy(s_info_layer);
 
   // Destroy canvas
   layer_destroy(s_canvas_layer);
@@ -365,6 +450,7 @@ static void main_window_unload(Window *window) {
   // Destroy the image data
   gbitmap_destroy(s_bitmap_sun_rim);
   gbitmap_destroy(s_bitmap_horizon);
+  gbitmap_destroy(s_bitmap_moon);
 }
 
 static void init() {
@@ -393,8 +479,11 @@ static void init() {
   s_bitmap_sun_rim = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SUN_RIM);
   s_bitmap_horizon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_HORIZON);
   
+  // get proper moon phase image
+  load_moon_image();
+  
   // calculate sun paths
-  sky_paths_today(64.8, -147, solar_elev, solar_azi, lunar_elev, lunar_azi, lunar_phase);
+  sky_paths_today(lat, lng, solar_elev, solar_azi, lunar_elev, lunar_azi);
 }
 
 static void deinit() {
